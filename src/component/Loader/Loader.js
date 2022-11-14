d3518ee15fd9ea57d0ce08bdfa62cbcd196dbe4e
Zile028 <zile028@gmail.com>
2022-11-14 13:53:55 +0100

    loader

---
+++ src/component/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./loader.scss"
+function Loader() {
+	return (
+	  <div className="loader">
+		  <div className="loader-circle">
+			  <img src="https://logos-world.net/wp-content/uploads/2020/04/Nike-Logo.png" alt=""/>
+		  </div>
+	  </div>
+	);
+}
+
+export default Loader;
--- src/pages/ShowRoom/ShowRoom.js
+++ src/pages/ShowRoom/ShowRoom.js
@@ -1,25 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 import ProductCard from "../../component/ProductCard/ProductCard";
 import "./showroom.scss";
+import Loader from "../../component/Loader/Loader";
 
 function ShowRoom() {
-  const { products } = useSelector((state) => state.productStore);
-  const { category } = useParams();
+	const {products} = useSelector((state) => state.productStore);
+	const {category} = useParams();
+	const [loader, setLoader] = useState(true)
+	useEffect(() => {
+		setTimeout(() => {
+			setLoader(false)
+		}, 2000)
+	}, [category]);
 
-  return (
-    <section className="showroom container">
-      <aside>
-        <h2>filter price</h2>
-      </aside>
-      <article>
-        {products[category].map((el, index) => {
-          return <ProductCard key={index} product={el} />;
-        })}
-      </article>
-    </section>
-  );
+
+	return (
+	  <section className="showroom container">
+		  <aside>
+			  <h2>filter price</h2>
+		  </aside>
+		  <article>
+			  {products[category].map((el, index) => {
+				  return <ProductCard key={index} product={el}/>;
+			  })}
+			  {loader && <Loader/>}
+		  </article>
+	  </section>
+	);
 }
 
 export default ShowRoom;
