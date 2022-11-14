57baa7f06b9a9a91e918b2c9d85a4723afe1469f
zile028 <zile028@gmail.com>
2022-11-14 08:12:27 +0100

    product page

--- src/component/ProductCard/ProductCard.js
+++ src/component/ProductCard/ProductCard.js
@@ -1,12 +1,24 @@
-import React from 'react';
-
-function ProductCard({product}) {
-    return (
-        <div className="product">
-            <img src={product.img} alt="product"/>
-            <p>{product.title}</p>
-        </div>
-    );
+import React from "react";
+import "./productcard.scss";
+function ProductCard({ product }) {
+  return (
+    <div className="product-card">
+      <div className="product-card-image">
+        <img src={product.img} alt="product" />
+      </div>
+      <div className="product-card-body">
+        <h3>{product.title}</h3>
+        <p className="price">{product.price}</p>
+        <p>{product.source}</p>
+      </div>
+      <div className="product-card-footer">
+        <a className="btn btn-primary btn-sm" href={product.url} target="blank">
+          Details
+        </a>
+        <button className="btn btn-danger btn-sm">Add to cart</button>
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
--- src/pages/ShowRoom/ShowRoom.js
+++ src/pages/ShowRoom/ShowRoom.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import {useSelector} from "react-redux";
-import {useParams} from "react-router-dom";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import ProductCard from "../../component/ProductCard/ProductCard";
+import "./showroom.scss";
 
 function ShowRoom() {
-    const {products} = useSelector((state) => state.productStore)
-    const {category} = useParams()
+  const { products } = useSelector((state) => state.productStore);
+  const { category } = useParams();
 
-    return (
-        <section className="container">
-            <aside>
-                <h2>filter price</h2>
-            </aside>
-            <article>
-                <h2>Products</h2>
-                {products[category].map((el, index) => {
-                    return <ProductCard key={index} product={el}/>
-                })}
-            </article>
-        </section>
-    );
+  return (
+    <section className="showroom container">
+      <aside>
+        <h2>filter price</h2>
+      </aside>
+      <article>
+        {products[category].map((el, index) => {
+          return <ProductCard key={index} product={el} />;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default ShowRoom;
