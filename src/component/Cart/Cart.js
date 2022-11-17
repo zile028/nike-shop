d842ec655171d8f3a2b5108d3d2f4b803697f95d
zile028 <zile028@gmail.com>
2022-11-17 08:19:05 +0100

    cart transition

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,62 +1,84 @@
-import React from "react";
-import {AiFillMinusCircle, AiFillPlusCircle} from "react-icons/ai";
-import {BsTrash} from "react-icons/bs";
-import {useDispatch, useSelector} from "react-redux";
-import {changeCount, removeFromCart} from "../../store/sliceCart";
+import React, { Fragment, useEffect, useState } from "react";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
+import { BsTrash } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { changeCount, removeFromCart } from "../../store/sliceCart";
 import "./cart.scss";
 
-function Cart({cart}) {
-	const {total} = useSelector((state) => state.cartStore)
-	const dispatch = useDispatch();
-	const quantityHandler = (increment, index) => {
-		dispatch(changeCount({increment, index}));
-	};
-	const removeHandler = (index) => {
-		dispatch(removeFromCart(index));
-	};
+function Cart({ cart, showCart, setIsMounted }) {
+  const { total } = useSelector((state) => state.cartStore);
+  const dispatch = useDispatch();
+  const [animate, setAnimate] = useState({});
+  useEffect(() => {
+    let timer = null;
+    if (showCart) {
+      setAnimate({ animationName: "slideIn" });
+    } else {
+      setAnimate({ animationName: "slideOut" });
+      timer = setTimeout(() => {
+        setIsMounted(false);
+      }, 400);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [showCart]);
 
-	return (
-	  <div className="cart">
-		  {cart.map((el, index) => {
-			  return (
-				<div className="cart-product" key={index}>
-					<div>
-						<img src={el.img} alt={el.title}/>
-					</div>
-					<div>
-						<h4>{el.title}</h4>
-						<p>${el.price}</p>
-						<div className="quantity">
-							<button
-							  onClick={() => {
-								  quantityHandler(1, index);
-							  }}
-							>
-								<AiFillPlusCircle/>
-							</button>
-							<span>{el.count}</span>
-							<button
-							  onClick={() => {
-								  quantityHandler(-1, index);
-							  }}
-							>
-								<AiFillMinusCircle/>
-							</button>
-						</div>
-					</div>
-					<button
-					  onClick={() => {
-						  removeHandler(index);
-					  }}
-					>
-						<BsTrash/>
-					</button>
-				</div>
-			  );
-		  })}
-		  <div className="cart-total"><p>Total: <span>${total}</span></p></div>
-	  </div>
-	);
+  const quantityHandler = (increment, index) => {
+    dispatch(changeCount({ increment, index }));
+  };
+  const removeHandler = (index) => {
+    dispatch(removeFromCart(index));
+  };
+
+  return (
+    <div className="cart" style={animate}>
+      <div className="cart-product">
+        {cart.map((el, index) => {
+          return (
+            <Fragment key={index}>
+              <div>
+                <img src={el.img} alt={el.title} />
+              </div>
+              <div>
+                <h4>{el.title}</h4>
+                <p>${el.price}</p>
+                <div className="quantity">
+                  <button
+                    onClick={() => {
+                      quantityHandler(1, index);
+                    }}
+                  >
+                    <AiFillPlusCircle />
+                  </button>
+                  <span>{el.count}</span>
+                  <button
+                    onClick={() => {
+                      quantityHandler(-1, index);
+                    }}
+                  >
+                    <AiFillMinusCircle />
+                  </button>
+                </div>
+              </div>
+              <button
+                onClick={() => {
+                  removeHandler(index);
+                }}
+              >
+                <BsTrash />
+              </button>
+            </Fragment>
+          );
+        })}
+      </div>
+      <div className="cart-total">
+        <p>
+          Total: <span>${total}</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,62 +1,71 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../logo.svg";
-import {Link, NavLink} from "react-router-dom";
-import {AiOutlineLogin, AiOutlineShoppingCart} from "react-icons/ai";
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineLogin, AiOutlineShoppingCart } from "react-icons/ai";
 import "./navigation.scss";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import Cart from "../Cart/Cart";
 
 function Navigation() {
-	const {category} = useSelector((state) => state.productStore);
-	const {cart} = useSelector((state) => state.cartStore);
-	const [showCart, setShowCart] = useState(false);
+  const { category } = useSelector((state) => state.productStore);
+  const { cart } = useSelector((state) => state.cartStore);
+  const [showCart, setShowCart] = useState(false);
 
-	useEffect(() => {
-		if (cart.length === 0) {
-			setShowCart(false);
-		}
-	}, [cart]);
+  const [isMounted, setIsMounted] = useState(false);
 
-	const renderNavLink = () => {
-		return category.map((el, index) => {
-			return (
-			  <li key={index}>
-				  <NavLink to={"/showroom/" + el.toLowerCase()}>{el}</NavLink>
-			  </li>
-			);
-		});
-	};
+  useEffect(() => {
+    if (cart.length === 0) {
+      setShowCart(false);
+    }
+  }, [cart]);
 
-	const showCartHandler = () => {
-		if (cart.length > 0) {
-			setShowCart(!showCart);
-		}
-	};
+  const renderNavLink = () => {
+    return category.map((el, index) => {
+      return (
+        <li key={index}>
+          <NavLink to={"/showroom/" + el.toLowerCase()}>{el}</NavLink>
+        </li>
+      );
+    });
+  };
 
-	return (
-	  <nav className="navbar">
-		  <div className="container">
-			  <div className="navbar-logo">
-				  <Link to={"/"}>
-					  <img src={logo} alt="nike"/>
-				  </Link>
-			  </div>
-			  <div className="navbar-menu">
-				  <ul>{renderNavLink()}</ul>
-			  </div>
-			  <div className="navbar-action">
-				  <button>
-					  <AiOutlineLogin/>
-				  </button>
-				  <button className="cart-icon" onClick={showCartHandler}>
-					  <AiOutlineShoppingCart/>
-					  {cart.length > 0 && <span>{cart.length}</span>}
-				  </button>
-				  {showCart && <Cart cart={cart}/>}
-			  </div>
-		  </div>
-	  </nav>
-	);
+  const showCartHandler = () => {
+    if (cart.length > 0) {
+      if (showCart) {
+        setShowCart(!showCart);
+      } else {
+        setIsMounted(true);
+        setShowCart(!showCart);
+      }
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-logo">
+          <Link to={"/"}>
+            <img src={logo} alt="nike" />
+          </Link>
+        </div>
+        <div className="navbar-menu">
+          <ul>{renderNavLink()}</ul>
+        </div>
+        <div className="navbar-action">
+          <button>
+            <AiOutlineLogin />
+          </button>
+          <button className="cart-icon" onClick={showCartHandler}>
+            <AiOutlineShoppingCart />
+            {cart.length > 0 && <span>{cart.length}</span>}
+          </button>
+          {isMounted && (
+            <Cart cart={cart} showCart={showCart} setIsMounted={setIsMounted} />
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
