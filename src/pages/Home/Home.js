6f9a5f45b660f168c132a2873aeb0a58984740a0
zile028 <zile028@gmail.com>
2022-11-25 08:08:21 +0100

    favorite slide

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import ProductCard from "../../component/ProductCard/ProductCard";
 import Products from "../../services/Products";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+// import "@splidejs/react-splide/css";
+import "@splidejs/react-splide/css/skyblue";
 
 function Home() {
   const { user } = useSelector((state) => state.userStore);
+  const [favorites, setFavorites] = useState([]);
   useEffect(() => {
     Products.getFavorits(user.favorites).then((data) => {
-      console.log(data);
+      setFavorites(data);
     });
   }, []);
   return (
@@ -18,7 +23,30 @@ function Home() {
         <article>
           <h2>favorits</h2>
         </article>
-        <article></article>
+        <article>
+          <Splide
+            options={{
+              type: "loop",
+              perPage: 3,
+              autoplay: true,
+              gap: "1rem",
+              rewind: true,
+              speed: 400,
+              interval: 1000,
+              arrows: false,
+              pagination: false,
+              pauseOnHover: true,
+            }}
+          >
+            {favorites.length > 0
+              ? favorites.map((el, index) => (
+                  <SplideSlide key={index}>
+                    <ProductCard product={el} />
+                  </SplideSlide>
+                ))
+              : null}
+          </Splide>
+        </article>
       </section>
     </>
   );
